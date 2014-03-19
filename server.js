a39a3e1d4a754b80f46111968aaed8f533cacb22
zac <zaccary.price@avansera.com>
2014-03-19 16:15:59 +0200

    query search

--- server.js
+++ server.js
@@ -1,51 +1,52 @@
 'use strict';
 
 var express = require('express');
-var ssh		= require('ssh2');
-var log		= require('npmlog');
-var q		= require('q');
+var ssh = require('ssh2');
+var log = require('npmlog');
+var q = require('q');
 var sshConn = require('./lib/config/sshConnect');
 log.enableColor();
-
 
 
 // connect to server via ssh
 
 sshConn()
 	.then(
-		function() {
-			log.info('SSH connection open');
-		}
-	);
+	function () {
+		log.info('SSH connection open');
 
+
+		/**
+		 * Main application file
+		 */
+
+			// Set default node environment to development
+		process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+		// Application Config
+		var config = require('./lib/config/config');
+		log.info("Configuration loaded");
+		var app = express();
+
+		// Express settings
+		require('./lib/config/express')(app);
+		log.info("express config loaded");
+
+		// Routing
+		require('./lib/routes')(app);
+		log.info("router loaded");
+
+		// Start server
+		app.listen(config.port, function () {
+			log.info('Express server listening on port ' + config.port + " in " + app.get('env') + " mode");
+		});
+
+		// Expose app
+		exports = module.exports = app;
+	}
+);
 
 
 // if successful resume with creating listen server
 
 
-
-/**
- * Main application file
- */
-
-// Set default node environment to development
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-// Application Config
-var config = require('./lib/config/config');
-
-var app = express();
-
-// Express settings
-require('./lib/config/express')(app);
-
-// Routing
-require('./lib/routes')(app);
-
-// Start server
-app.listen(config.port, function () {
-  console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
-});
-
-// Expose app
-exports = module.exports = app;
