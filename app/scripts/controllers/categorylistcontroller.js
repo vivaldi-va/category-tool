10d639da8a1c7a2a44e44570730db06b719180e8
zac <zaccary.price@avansera.com>
2014-04-04 14:49:23 +0300

    list styles and product search toolbar

--- app/scripts/controllers/categorylistcontroller.js
+++ app/scripts/controllers/categorylistcontroller.js
@@ -4,10 +4,29 @@
 
 angular.module('Cat.Controllers')
 	.controller('CatListCtrl', ['$scope', '$log', 'CategoryService', function($scope, $log, CategoryService) {
-		$scope.categories = CategoryService.get();
+		$scope.categories		= CategoryService.get();
+
+		$scope.categories.$promise.then(function(data) {
+			$scope.categoryRoot		= data.root;
+		});
+
+
+		$scope.expandedCategory	= null;
+
+
+		$scope.expandCategory = function(elem) {
+			if($scope.expandedCategory === null || $scope.expandedCategory !== elem) {
+				$scope.expandedCategory = elem;
+			} else {
+				$scope.expandedCategory = null;
+			}
+		};
+
+		$scope.navigateDown = function(category) {
+			$scope.categoryRoot = category;
+		};
 
 		$scope.makeCategory = function(parent, name) {
 			parent = parent || 0;
-
 		};
 	}]);
